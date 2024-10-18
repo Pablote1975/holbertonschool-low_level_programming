0a1adfea5c0b4dcf7bbf66755dcaae2170312707
Pablote1975 <10055@holbertonstudents.com>
2024-10-18 19:30:51 +0000

    

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - estoy aprendiendo C
+ *()
+ *Return: 0
+ */
+
+int main(void)
+{
+char *tc_4 = "\"Programming is like building a multilingual puzzle" ;
+puts(tc_4);
+
+
+return (0);
+}
