abafd3f3084041ef40a787b8e3c4d2d5195463c3
Pablote1975 <10055@holbertonstudents.com>
2024-10-21 21:59:53 +0000

    Tc1 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ precio = n % 10;
 printf("Last digit of %d is ", n);
 if (precio > 5)
 printf("%d and is greater than 5\n", precio);
-else if (precio < 6)
+else if (precio < 6 && precio != 0)
 printf("%d and is less than 6 and not 0\n", precio);
 else
 printf("%d and is 0\n", precio);
