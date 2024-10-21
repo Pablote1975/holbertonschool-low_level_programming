fc55bf678307a227b61e7daab9a5d34f83d5b904
Pablote1975 <10055@holbertonstudents.com>
2024-10-21 22:57:20 +0000

    T3c 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *main - Moneda 3 de C T2
+ *Return: 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+
+return (0);
+}
