a0551ff95ee17dd6d2d0051aefba5a47a0eb99b5
Pablote1975 <10055@holbertonstudents.com>
2024-10-22 18:32:58 +0000

    T3c 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main- last digit %
+*Moneda 3 task 5
+*Return:0
+*/
+
+int main(void)
+{
+int decimos;
+for (decimos = 0; decimos <= 10; decimos++)
+{
+printf("%d", decimos);
+}
+printf("\n");
+return (0);
+}
