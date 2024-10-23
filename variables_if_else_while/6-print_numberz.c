57e9c5f7101e6aa06fdf53e2f712738244e656a4
Pablote1975 <1oo50@holbertonschool.com>
2024-10-23 04:23:07 +0000

    T2c 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main- last digit %
+*Moneda 2 task 6
+*Return:0
+*/
+
+int main(void)
+{
+int decimos;
+for (decimos = 0; decimos < 10; decimos++)
+{
+putchar(decimos);
+}
+putchar('\n');
+return (0);
+}
