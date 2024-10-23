d6fc083d14c87acc771d84b825b913d628379d38
Pablote1975 <10055@holbertonstudents.com>
2024-10-23 18:20:24 +0000

    T2c 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ **main - Moneda 2 de C T8
+ *Return: 0
+ */
+
+int main(void)
+{
+int hx;
+
+for (hx = 0; hx < 16; hx++)
+{
+if (hx < 10)
+}
+putchar(i + '0');
+}
+else
+{
+putchar(hx - 10 + 'a');
+}
+}
+putchar('\n');
+
+return (0);
+}
