8eb6a929b1c11eb953612d1d1387fab48afca26e
Pblote1975 <10055@holbertonstudents.com>
2024-10-25 13:25:34 +0000

    T3c 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - Moneda 3 de C T1
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+return (0);
+}
