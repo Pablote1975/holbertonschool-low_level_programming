9bf0372593763f87f0868ac7ffc80134a82559d4
Pblote1975 <10055@holbertonstudents.com>
2024-10-25 18:49:39 +0000

    T3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
-*int _islower(int c) - Moneda 3 de C T3
- *Return: 0
+*_islower: - Moneda 3 de C T3
+*@c: Variable desconocida
+*
+*Return: 0
 */
-int _islower(int c);
 
+int _islower(int c)
+{
 if (islower(c))
 return (1);
 else
 return (0);
+}
