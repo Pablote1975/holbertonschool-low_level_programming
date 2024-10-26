63a0f3bfc6952a4fd3f3eeabf88dc657dc15e02c
Pblote1975 <10055@holbertonstudents.com>
2024-10-26 01:33:49 +0000

    T3

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <time.h>
+/**
+*jack_bauer- Moneda 3 de C
+*: Variable desconocida
+*Return: 0
+*/
+
+void jack_bauer(void)
+{
+int h;
+int m;
+
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+	  _putchar((h / 10) + '0');
+	  _putchar((h % 10) + '0');
+	  _putchar(':');
+	  _putchar((m / 10) + '0');
+	  _putchar((m % 10) + '0');
+	  _putchar('\n');
+}
+}
+}
+
+int main(void)
+{
+jack_bauer();
+return (0);
+}
