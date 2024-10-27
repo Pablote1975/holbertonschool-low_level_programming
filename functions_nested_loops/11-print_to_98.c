62428655de6fe3c7f9c13421d87d7bd350fa93db
Pblote1975 <10055@holbertonstudents.com>
2024-10-27 08:59:39 +0000

    T3

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *print_to_98- Moneda 3 C
  *@n: numeros
@@ -10,7 +11,6 @@ void print_to_98(int n)
 {
 int i;
 if (n < 98)
-{
 {
 for (i = n; i <= 98; i++)
 {
