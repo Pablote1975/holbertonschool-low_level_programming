90ff74ed11ef1637a239f7e7acc184202cc0300a
Pblote1975 <10055@holbertonstudents.com>
2024-10-28 18:43:40 +0000

    T4

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+_isupper(int c);
+_isdigit(int c);
+mul(int a, int b);
+print_numbers(void);
+print_most_numbers(void);
+more_numbers(void);
+print_line(int n);
+print_diagonal(int n);
+print_square(int size);
+print_triangle(int size);
+
+
