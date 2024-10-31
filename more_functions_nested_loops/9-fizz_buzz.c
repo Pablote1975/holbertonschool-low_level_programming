50cb3bb2e4896f70ab02d53ec2764f23a0fdacc4
Pblote1975 <10055@holbertonstudents.com>
2024-10-31 18:05:29 +0000

    T4

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main  - moneda 4
+*:la variable prestablecida
+*Return:0
+*/
+
+int main(void)
+{
+int f;
+for (f = 1; f <= 100; f++)
+if (f % 3 && f % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (f % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (f % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d", f);
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
