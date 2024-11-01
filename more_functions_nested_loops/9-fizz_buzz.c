d9c589b101c7d3d390e2e0b9ec6c51188b64f292
Pblote1975 <10055@holbertonstudents.com>
2024-11-01 12:41:52 +0000

    t4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,6 +10,7 @@ int main(void)
 {
 int f;
 for (f = 1; f <= 100; f++)
+{
 if (f % 3 == 0 && f % 5 == 0)
 printf("FizzBuzz");
 else if (f % 3 == 0)
@@ -18,10 +19,11 @@ else if (f % 5 == 0)
 printf("Buzz");
 else
 {
-printf("%d ", f);
+printf("%d", f);
+}
+if (f < 100)
 putchar(' ');
 }
- 
 putchar('\n');
 return (0);
 }
