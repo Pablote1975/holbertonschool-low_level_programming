6d84bcb3b022882cc05398517b7868c8ff34481b
Pblote1975 <10055@holbertonstudents.com>
2024-11-01 16:33:02 +0000

    T4

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*print_triangle- moneda 4
+*@size:la variable prestablecida
+*Return:0
+*/
+
+void print_triangle(int size)
+{
+int i, k, esp;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (esp = 0; esp < size - i; esp++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+ }
+}
