7f532b6565492524960562a2628601d694b71e7e
Pblote1975 <10055@holbertonstudents.com>
2024-11-04 17:40:16 +0000

    M13

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,33 @@
 #include "main.h"
-#include <string.h>
-
 /**
  *puts_half- Moneda 13
  *@str:variable
  *Return:lenght
 */
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
 
 void puts_half(char *str)
 {
-int i = 0;
-int len = strlen(str);
-int n = (len - 1) / 2;
+	int i = 0;
+	int len = _strlen(str);
+	int n = (len - 1) / 2;
 
-if (len % 2 != 0)
-{
-n++;
-}
-for (i = n; i < len; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	if (len % 2 != 0)
+	{
+		n++;
+	}
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *print_array- Moneda 13
+ *@s:variable
+ *Return:lenght
+*/
+
+void print_array(int *a, int n)
+{
+  for n[]
