59c3f4489edd3f219cc9aee7a8db671545bf00a2
Pblote1975 <10055@holbertonstudents.com>
2024-11-05 17:33:20 +0000

    M13

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,17 +3,16 @@
 /**
  *print_array- Moneda 13
  *@n:variable
- *@a:variable
+ *
  *Return:dest
 */
 char *_strcpy(char *dest, char *src)
 {
-  int n = 0;
-  for (; src[n] != '\0'; n++)
-    {
-      dest[n] = src[n];
-    }
-  dest[n] = '\0';
-
-  return (dest);
+int n = 0;
+for (; src[n] != '\0'; n++)
+{
+dest[n] = src[n];
+}
+dest[n] = '\0';
+return (dest);
 }
