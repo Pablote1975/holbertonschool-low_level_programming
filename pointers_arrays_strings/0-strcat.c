5649b408b9b98c9e688e6bf018803c742f46daf9
Pblote1975 <10055@holbertonstudents.com>
2024-11-05 21:19:54 +0000

    M14

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,21 @@
 
 /**
  *_strcat- M14
- *
- *
+ *@dest:varia
+ *@src:varia
  *Return:
  */
 char *_strcat(char *dest, char *src)
 {
-  for 
+  char *n = dest;
+
+   for (*dest = 0; *dest != '\0'; dest++)
+     {
+     }  
+    for (*src = 0; *src != '\0'; src++)
+{      *dest = *src;
+  dest++;
+      }
+*dest = '\0';
+ return (n);
+}
