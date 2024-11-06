6005e9878077ae8ca9c6192992633be5dc922a58
Pblote1975 <10055@holbertonstudents.com>
2024-11-06 15:37:50 +0000

    M14

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,18 +5,14 @@
  *@dest:varia
  *@src:varia
  *@n:varia
- *Return:
+ *Return:dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int l;
-  for (l = 0; l < n && src[l] != '\0'; l++)
-
-      dest[l] = src[l];
-  while (l < n)
-    {
-      *dest++ = '\0';
-      l++;
-    }
-  return (dest); 
+int l;
+for (l = 0; l < n && src[l] != '\0'; l++)
+dest[l] = src[l];
+for (; l < n; l++)
+dest[l] = '\0';
+return (dest); 
 }
