609010ac8661db4b0245b61894f2ada9a87e4ff3
Pblote1975 <10055@holbertonstudents.com>
2024-11-06 15:13:17 +0000

    M14

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strcat- M14
+ *@dest:varia
+ *@src:varia
+ *@n:varia
+ *Return:
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+  int l;
+  for (l = 0; l < n && src[l] != '\0'; l++)
+
+      dest[l] = src[l];
+  return (dest); 
+}
