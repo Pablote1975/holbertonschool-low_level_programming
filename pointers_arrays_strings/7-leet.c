77dabfb81b80fba5ffd5d0ea15037301cb3aaeb3
Pblote1975 <10055@holbertonstudents.com>
2024-11-07 19:20:33 +0000

    M14

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,24 @@
  */
 char *leet(char *t)
 {
-char *nums = "43071";
- char *let = "aeotlAEOTL";
- int i = 0;
- int j = 0;
- 
- for (; t[i]; i++)
-   {
-     for (; let[j]; j++)
-       {
-	 if (t[i] == let[j])
-	   {t[i] = nums[j];
-	     break;
-	       }
+char num[5] = "43071";
+char let[5] = "AEOTL";
+int i = 0;
+int j = 0;
+int l = 5;
+
+while (t[i])
+{
+j = 0;
+while (j < l)
+{
+if (t[i] == let[j] || t[i] - 32 == let[j])
+{
+t[i] = num[j];
 }
+j++;
+}
+i++;
 }
 return (t);
 }
