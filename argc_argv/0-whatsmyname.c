4b7e8b1ffadf6abe6ec345349a9a79b587a4bb51
Pblote1975 <10055@holbertonstudents.com>
2024-11-09 06:26:45 +0000

    M16

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+* main - prints its name, followed by a new line
+ * @argc: segun piden no hace nada
+ * @argv: es lo que devuelve el nombre del programa
+ * Return: el nombre del programa
+ */
 int main(int argc, char *argv[])
 {
 (void)argc;
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
