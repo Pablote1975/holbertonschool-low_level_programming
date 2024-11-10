0f76e8794a909d9fc303b7fcd25acf5e571a5489
Pblote1975 <10055@holbertonstudents.com>
2024-11-10 00:47:36 +0000

    M13

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,22 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* main - prints its number and new line
+* main - print and new line
  * @argc: lo que piden
  * @argv: nombre del comando
  * Return: numbers
  */
+
 int main(int argc, char *argv[])
 {
 int i;
-int j;
+
 for (i = 0; i < argc; i++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-_putchar(argv[i][j]);
-}
-_putchar('\n');
+  printf("%s\n", argv[i]);
 }
 return (0);
 }
