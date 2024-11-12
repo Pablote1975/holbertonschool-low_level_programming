179cf45b367f409644d66c41fa124c3ce61d8a14
Pblote1975 <10055@holbertonstudents.com>
2024-11-12 16:22:12 +0000

    M17

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,22 +9,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *array = (char *)malloc(size * sizeof(char));  
+unsigned int i;
+char *array = malloc(size * sizeof(char));
 
-  if (size == 0)
-      {
-        return NULL;
-    }
-
-      if (array == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < size; i++)
-      {
-        array[i] = c;
-    }
-
-    return array;
+if (size == 0)
+{
+return (NULL);
+}
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
 }
