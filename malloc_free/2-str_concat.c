a977fbdaf58b5386c48f5167c5cd51564266ee33
Pablote1975 <10055@holbertonstudents.com>
2024-11-14 04:46:31 +0000

    M18

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *str_concat- Moneda 18
+ *
+ *@s1: varaible
+ *@s2: variable
+ *Return: grid
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *result;
+size_t len1;
+size_t len2;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+len1 = strlen(s1);
+len2 = strlen(s2);
+
+result = (char *)malloc(len1 + len2 + 1);
+
+if (result == NULL)
+{
+return (NULL);
+}
+strcpy(result, s1);
+strcat(result, s2);
+
+return (result);
+}
