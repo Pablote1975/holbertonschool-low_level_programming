99dd51b88191a5f4091342402cf4e90fab44134b
Pblote1975 <10055@holbertonstudents.com>
2024-11-18 05:34:28 +0000

    M20

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <string.h>
 #include <stdlib.h>
 /**
  * new_dog- Moneda 20
@@ -12,19 +11,23 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int i;
 dog_t *dog = malloc(sizeof(dog_t));
 if (!dog)
 return (NULL);
 
-dog->name = malloc(strlen(name) + 1);
-dog->owner = malloc(strlen(owner) + 1);
-if (!dog->name || !dog->owner)
+for (i = 0; name[i] != '\0'; i++)
 {
-free(dog->name);
-free(dog->owner);
-free(dog);
-return (NULL);
+dog->name[i] = name[i];
 }
+dog->name[i] = '\0';
+
+for (i = 0; owner[i] != '\0'; i++)
+{
+dog->owner[i] = owner[i];
+}
+dog->owner[i] = '\0';
+
 dog->age = age;
 return (dog);
 }
