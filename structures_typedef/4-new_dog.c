7c538699201f1199c7eec39e454a0568989f703c
Pblote1975 <10055@holbertonstudents.com>
2024-11-18 07:05:19 +0000

    M20

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,14 +11,17 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i;
+int i, larname = 0, larowner = 0;
 dog_t *dog = malloc(sizeof(dog_t));
+
 if (!dog)
 return (NULL);
-
-dog->name = malloc(25);
-dog->owner = malloc(25);
-
+{
+while (name[larname] != '\0') larname++;
+while (owner[larowner] != '\0') larowner++;
+dog->name = malloc(larname + 1);
+dog->owner = malloc(larowner + 1);
+}
 for (i = 0; name[i] != '\0'; i++)
 {
 dog->name[i] = name[i];
