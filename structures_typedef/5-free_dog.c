81de9711ef81128a478474070858dcacf9cc654f
Pablote1975 <10055@holbertonstudents.com>
2024-11-21 06:05:10 +0000

    M20

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ **new_dog- Moneda 20
+ *@name: name
+ *@age: age
+ *@owner: owner
+ * variable
+ *return:dog
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
